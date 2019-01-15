482c959d9a9820ad7664c54bb14cdddcce2515ce
radarworld <45634039+radarworld@users.noreply.github.com>
2019-01-15 16:03:42 +0100

    Add files via upload

--- tkinterBasicGUI.py
+++ tkinterBasicGUI.py
@@ -1,5 +1,6 @@
 from tkinter import *
-from tkinter import Tk, Frame, Menu, messagebox
+from tkinter import Tk, Frame, Menu, messagebox, W, E, N, S
+from tkinter.ttk import Separator, Style
 from tkinter.scrolledtext import ScrolledText
 import sys
 
@@ -31,9 +32,14 @@ class MainWindow(Frame):
         #set topWindow to XGA+ resolution
         topWindow.geometry('%dx%d' % (1152, 864))
         topWindow.minsize(1152,864)
-        #label = tkinter.Label(self, text="Hello")
-        #label.pack()
-        #label.bind("<1>", self.quit)
+
+        topWindow.columnconfigure(0, weight=1)
+        topWindow.columnconfigure(1, weight=1)
+        topWindow.rowconfigure(0, weight=90)
+        topWindow.rowconfigure(1, weight=3)
+        topWindow.rowconfigure(2, weight=1)
+        self.grid(sticky = W+E+N+S)
+        
 
         self.master.title("Simple Demo")
 
@@ -45,7 +51,7 @@ class MainWindow(Frame):
         menubar.add_cascade(label="File", menu=fileMenu)
         fileMenu.add_command(label="Open", command=MainWindow.doNothing)        
         fileMenu.add_separator()
-        fileMenu.add_command(label="Exit", command=MainWindow.doNothing)
+        fileMenu.add_command(label="Exit", command=MainWindow.quit)
 
         editMenu = Menu(menubar, tearoff=0)
         menubar.add_cascade(label="Edit", menu=editMenu)
@@ -55,24 +61,28 @@ class MainWindow(Frame):
         menubar.add_cascade(label="Help", menu=helpMenu)
         helpMenu.add_command(label="About", command=MainWindow.helpMenu_About)
 
-        scrolled_text = ScrolledText(topWindow, state='disabled', height=5)
-        scrolled_text.configure(font='TkFixedFont')
-        scrolled_text.tag_config('INFO', foreground='black')
-
 
         spaceHolder = Label(topWindow, text="radio buttons", bd=1, relief=SUNKEN, anchor=W)
         spaceHolder.grid(row=0, column=0)
+                
+        spaceHolder2 = Label(topWindow, text="graph", bd=4, relief=SUNKEN, anchor=W)
+        spaceHolder2.grid(row=0, column=1, sticky = W+E+N+S)
 
-
-        spaceHolder2 = Label(topWindow, text="graph", bd=1, relief=SUNKEN, anchor=E)
-        spaceHolder2.grid(row=0, column=1)
+        scrolled_text = ScrolledText(topWindow, state='disabled', height=5)
+        scrolled_text.configure(font='TkFixedFont')
+        scrolled_text.tag_config('INFO', foreground='black')
+        scrolled_text.grid(row=1, column=0, columnspan=2, sticky = W+E+N+S)
 
         
 
-        statusBar = Label(topWindow, text="status bar...", bd=1, relief=SUNKEN, anchor=W)
-        statusBar.grid(row=1, column=0, columnspan=1)
+        statusBar = Label(topWindow, text="status bar....", bd=1, relief=SUNKEN, anchor=W)
+        statusBar.grid(row=2, column=0, columnspan=2, sticky = W+E+N+S)
 
-        
+        #sep = Separator(topWindow, orient="vertical")
+        #sep.grid(row=0, column=1, sticky="ns")
+
+
+
 
 
 
@@ -83,8 +93,8 @@ class MainWindow(Frame):
     def doNothing():
         pass
     
-    def quit(sel, event=None):
-        sys.exit()
+    def quit():
+        root.destroy()
 #Tk is a class and it will be copied to the root variable. So Tk object can be referreed as root variable.        
 #The command below also creates the root window.
 root = Tk()
