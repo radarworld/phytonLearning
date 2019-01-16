002961893706009819fb0443a96cf1bb9e8c7a5a
radarworld <45634039+radarworld@users.noreply.github.com>
2019-01-16 15:13:44 +0100

    Add files via upload

---
+++ radarControls.py
@@ -0,0 +1,14 @@
+import datetime
+
+class radarSystemClass(object):
+    def getSystemTime(self):        
+        return datetime.datetime.now()
+
+class updateLog(object):
+    def updateLogText(self):
+        self.text="eewrr"
+        return ("self.text")
+    
+
+    
+    
